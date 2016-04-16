f8ac65623d24ac74a91054a5fad157df399f6a6b
tetro <tetro93@gmail.com>
2016-04-16 14:21:45 +1000

    Base Project

---
+++ app/src/main/java/com/tetro93/myapp1/main_activity.java
@@ -0,0 +1,16 @@
+package com.tetro93.myapp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tetro on 16.04.2016.
+ */
+public class main_activity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
